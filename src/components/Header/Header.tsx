5c2176b3213bca7571203e5e79c1e73a9668a24b
OLGA ZELENKO <olichkazelenko@gmail.com>
2026-01-21 13:27:43 +0000

    add login and register form

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,29 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { NavLink } from "react-router-dom";
 
-const Header = () => {
+interface HeaderProps {
+  isAuth: boolean;
+  favoritesCount: number;
+  onLogout: () => void;
+}
+const Header = ({ isAuth, favoritesCount, onLogout }: HeaderProps) => {
+  const [showLogin, setShowLogin] = useState(false);
+  const [showRegister, setShowRegister] = useState(false);
+
+  useEffect(() => {
+    const handleEsc = (e: KeyboardEvent) => {
+      if (e.key === "Escape") {
+        setShowLogin(false);
+        setShowRegister(false);
+      }
+    };
+    window.addEventListener("keydown", handleEsc);
+    return () => {
+      window.removeEventListener("keydown", handleEsc);
+    };
+  }, []);
+
   return (
     <header>
       <div className="container">
@@ -10,7 +32,9 @@ const Header = () => {
             <svg></svg>
           </div>
         </Link>
+
         {/* Navigation*/}
+
         <nav className="navigation">
           <NavLink className="nav-link" to="/">
             Home
@@ -18,11 +42,55 @@ const Header = () => {
           <NavLink className="nav-link" to="/teachers">
             Teachers
           </NavLink>
-          <NavLink className="nav-link" to="/favorites">
-            Favorites
-          </NavLink>
+
+          {isAuth && favoritesCount > 0 && (
+            <NavLink className="nav-link" to="/favorites">
+              Favorites ({favoritesCount})
+            </NavLink>
+          )}
         </nav>
+
+        <div className="auth-buttons">
+          {!isAuth ? (
+            <>
+              <button onClick={() => setShowLogin(true)}>Login</button>
+              <button onClick={() => setShowRegister(true)}>Register</button>
+            </>
+          ) : (
+            <button onClick={onLogout}>Logout</button>
+          )}
+        </div>
       </div>
+
+      {/* modals for login and register */}
+
+      {showLogin && (
+        <div
+          className="modal"
+          onClick={(e) => e.target === e.currentTarget && setShowLogin(false)}
+        >
+          <div className="modal-content">
+            <button onClick={() => setShowLogin(false)}>🅧</button>
+            <h2>Login Form</h2>
+            {/* Login form react-hook-form*/}
+          </div>
+        </div>
+      )}
+
+      {showRegister && (
+        <div
+          className="modal"
+          onClick={(e) =>
+            e.target === e.currentTarget && setShowRegister(false)
+          }
+        >
+          <div className="modal-content">
+            <button onClick={() => setShowRegister(false)}>🅧</button>
+            <h2>Register Form</h2>
+            {/* Register form react-hook-form*/}
+          </div>
+        </div>
+      )}
     </header>
   );
 };
