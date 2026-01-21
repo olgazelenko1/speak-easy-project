e51b1b8a4923df2b982b303b2960943270c23f5b
OLGA ZELENKO <olichkazelenko@gmail.com>
2026-01-21 16:29:23 +0000

    add main files

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,8 +1,8 @@
-import Header from "../Header/Header";
+import Header from "../Header/Header.tsx";
 import { Outlet } from "react-router-dom";
 const Layout = () => {
   return (
-    <div className="layout">
+    <div className="theme-yellow">
       <Header />
       <Outlet />
     </div>
